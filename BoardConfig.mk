8c3b0a055537a0099f059cf339d2e6c92fe7038e
xpt1x <jatinsarda007@gmail.com>
2025-05-07 20:06:36 +0530

    feat: support custom kernel
    Change-Id: I6bfaf718aa7fb8a671f1b5df5d87c74d283a2837

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_munch
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_munch
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/munch.config
+# TARGET_KERNEL_CONFIG += vendor/xiaomi/munch.config
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := munch,munch_global,munch_in
